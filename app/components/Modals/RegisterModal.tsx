9ee99396625dfa5d8f9bb95cb3539382eb06b343
Todor <t.vlasev@gmail.com>
2023-05-05 00:42:12 +0300

    minor

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useCallback, useState } from "react";
+import { FC, useState } from "react";
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
